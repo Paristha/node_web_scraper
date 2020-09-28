68107b18db6ddac85163b3f3f3a66c6730243cdd
Thana Paris <raphaelparisdi@gmail.com>
2020-09-28 12:56:51 -0400

    hello world written
    hello world program node.js

---
+++ hello_world_website.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at PORT:${port}/`);
+});
